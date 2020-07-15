8bb1f7e332a27bb39cede8e623831a15e565f8b8
David Pujadas <dpujadas@gmail.com>
2020-07-15 23:25:59 +0200

    Review dashboard.js

--- src/locales/es/dashboard.js
+++ src/locales/es/dashboard.js
@@ -17,18 +17,18 @@
  */
 
 export default {
-  DASHBOARD_TITLE: 'Hola {nombre de usuario}',
-  DASHBOARD_BANNER_TITLE: 'Descripción general de la plataforma',
+  DASHBOARD_TITLE: 'Hola {username}',
+  DASHBOARD_BANNER_TITLE: 'Visión general de la plataforma',
   DASHBOARD_BANNER_DESC: 'Estadísticas agregadas sobre los recursos en ejecución y el uso de la plataforma actual.',
-  DASHBOARD_DESC: 'El tablero de instrumentos proporciona el uso de varios recursos en el clúster actual.',
+  DASHBOARD_DESC: 'El dashboard proporciona información sobre el uso de varios recursos en el clúster actual.',
   NODE_UNIT: 'Nodos',
   USAGE_VALUE: 'Uso',
   USAGE_RATE: 'Uso',
   NO_PROJECT: 'No se han encontrado proyectos disponibles.',
-  NO_POD_MONITOR_DATA: 'No hay datos de monitoreo de pods',
-  RESOURCE_WARNING_TIPS: 'Hay {warnNum} {tipName} se muestra de forma anormal.',
-  CPU_AVERAGE_LOAD: 'Promedio de carga',
-  USER_DASHBOARD_EMPTY_TITLE: 'No pertenece a ningún espacio de trabajo actualmente.',
-  USER_DASHBOARD_EMPTY_DESC: 'Puede ponerse en contacto con un administrador del espacio de trabajo para invitarlo a unirse a un espacio de trabajo.',
+  NO_POD_MONITOR_DATA: 'No hay datos de monitorización de pods',
+  RESOURCE_WARNING_TIPS: 'Hay {warnNum} {tipName} que se muestran anormalmente.',
+  CPU_AVERAGE_LOAD: 'Load Average',
+  USER_DASHBOARD_EMPTY_TITLE: 'No perteneces a ningún espacio de trabajo actualmente.',
+  USER_DASHBOARD_EMPTY_DESC: 'Puedes ponerte en contacto con un administrador del espacio de trabajo para que te invite a unirte.',
 
 }
