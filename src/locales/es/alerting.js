aca0d7a71a22d5458f37747898d5e0dc6ce4dec4
Antonio <antonio@geko.cloud>
2020-07-17 17:39:33 +0200

    /es/alerting.js traducido

--- src/locales/es/alerting.js
+++ src/locales/es/alerting.js
@@ -117,62 +117,90 @@ export default {
     'Unavailable statefulset replicas ratio',
   'View Details': 'View Details',
 
-  load1: 'Carga de la CPU promedio 1 minuto',
-  load5: 'Promedio de carga de la CPU 5 minutos',
-  load15: 'Promedio de carga de la CPU 15 minutos',
-  ALERT_TYPE: '{type} Alerta',
-  ALERT_POLICY_TYPE: '{type} Política de alertas',
-  ALERT_POLICY_DESC: 'Establecer reglas de alerta',
-  ALERT_MESSAGE_DESC: 'Los mensajes de alerta se generan en función de las métricas de monitoreo y las políticas de alerta de carga de trabajo en el proyecto actual. Pueden ayudar a los usuarios a detectar problemas y responder a tiempo.',
-  ALERT_POLICY_TRIGGER_RULE: 'Reglas de activación: se cumple cualquiera de las siguientes condiciones.',
-  ALERT_METRIC_NAME: 'Nombre métrico',
-  ALERT_COMMENT: 'Comentario',
-  ALERT_COMMENT_DESC: 'Los comentarios se conservarán como registros para el procesamiento de alertas, pero no como un medio para manejar las alertas. La información de registro se utilizará para verificar el estado de procesamiento de las alertas.',
-  ALERT_POLICY_SETTING_TITLE: 'La política de alerta relevante no se establece temporalmente',
-  ALERT_POLICY_SETTING_DESC: 'Puede encontrar la condición anormal del recurso a tiempo estableciendo las reglas de alerta',
-  ALERTING_POLICY_CREATE_DESC: 'Puede encontrar la condición anormal del recurso a tiempo estableciendo las reglas de alerta',
-  'ALERTING-POLICY_BASEINFO_DESC': 'Establecer información básica sobre la política de alerta',
-  'ALERTING-POLICY_MONITORING-TARGET_DESC': 'Seleccione el objetivo de supervisión de la política de alertas.',
-  'ALERTING-POLICY_ALERTING-RULE_DESC': 'Establecer reglas de alerta',
-  'ALERTING-POLICY_NOTIFICATION-RULE_DESC': 'Establecer reglas de notificación',
-  TOTAL_POLICIES: 'Total {num} políticas de alerta',
-  PERIOD_OPTIONS: '{valor, plural, = 1 {1 minuto / período} otro {# minutos / período}}',
-  CONSECUTIVE_OPTIONS: '{valor} veces consecutivas',
-  MAX_SEND_NOT_LIMIT: 'No limitado',
-  MAX_SEND_COUNT: 'Reenviar hasta {count}',
-  REPEAT_CUSTOM_TITLE: 'Personalizar reglas de repetición',
-  REPEAT_CUSTOM_DESC: 'Los mensajes de alerta se enviarán repetidamente a intervalos específicos según su gravedad. La política predeterminada se adoptará a menos que realice algún cambio en ella.',
-  REPEAT_INTERVAL_NOT_REPEAT: 'No repetir',
-  REPEAT_INTERVAL_MINUTE: 'Alerta una vez cada {num} minutos',
-  REPEAT_INTERVAL_HOUR: 'Alerta una vez cada {num} hora',
-  REPEAT_INTERVAL_DAY: 'Alerta una vez cada {num} días',
-  REPEAT_INTERVAL_EXP: 'Periodo exponencial incremental',
-  REPEAT_RULE_EXP_TIP: 'Notificación de incremento de índice periódico: cuando la duración de la alerta alcanza 1, 2, 4, 8, 16, 32 ... veces del período de estadísticas de alerta, se envía una notificación de alerta.',
-  REPEAT_RULE_TIPS: 'Los mensajes de alerta se enviarán repetidamente según su gravedad. Por defecto, <strong>la alerta crítica se repite cada 30 minutos</strong> / <strong>la alerta mayor se repite cada 2 horas</strong> / la <strong>alerta menor no se repite</strong> . <br> Si necesita modificar las reglas de repetición, personalice la configuración en la sección Regla de notificación (siguiente sección).',
-  NOTIFY_TIME_LABEL: 'Rango de tiempo de notificación efectivo',
-  NOTIFY_TIME_TIP: 'Puede establecer un rango de tiempo efectivo para un mensaje de alerta.',
-  NOTIFY_LIST_ADD_TIP: 'Presione el botón enter o haga clic para agregar',
-  NOTIFY_LIST_INPUT_ERRPR_TIP: 'El formato de entrada es incorrecto. Por favor, introduzca la dirección de correo electrónico correcta.',
-  NOTIFY_LIST_INPUT_PLACEHOLDER: 'Ingrese la dirección de correo electrónico del miembro para ser notificado.',
-  NOTIFY_CURRENT_COUNT: 'Tiempos de notificación: {count} en total',
-  RESOURCE_SELECTOR_FORM_TIP: 'Por favor complete el selector de etiquetas',
-  RESOURCE_NODE_FORM_TIP: 'Seleccione un nodo de clúster',
-  RESOURCE_WORKLOAD_FORM_TIP: 'Por favor seleccione una carga de trabajo',
-  SEVERITY_MSG_FIXED_MINUTES: 'Notificar cada {cuenta} minutos',
-  SEVERITY_MSG_NO_LIMIT: 'No limitado',
-  SEVERITY_MSG_EXP: 'Aviso por periodo exponencial',
-  SEVERITY_MSG_NOT_REPEAT: 'No hay notificaciones repetidas',
-  ALERT_RULE_STATUS_OCCURRED: 'Alertando',
-  ALERT_RULE_STATUS_NO_OCCURRED: 'Aún no alerta',
-  SENT_RULE_TIME_TITLE: 'Periodo de tiempo efectivo',
-  SENT_RULE_CHANNEL_TITLE: 'Método de notificación',
-  REQUESTS_FOR_TRIGGER_AN_ALARM_Q: '¿Requisitos previos para activar un mensaje de alerta?',
-  REQUESTS_FOR_TRIGGER_AN_ALARM_A: 'Debe establecer una política de alerta para un recurso. Cuando un determinado indicador de un recurso alcanza el umbral de la política de alerta, el mensaje se activa y empuja.',
-  REQUESTS_FOR_PUSH_AN_ALARM_Q: '¿Requisitos previos para un mensaje push de política de alerta?',
-  REQUESTS_FOR_PUSH_AN_ALARM_A: 'El administrador de la plataforma debe configurar el servidor de correo.',
-  HOW_TO_SUPRESS_AN_ALARM_Q: '¿Cómo suprimir mensajes de alerta?',
-  HOW_TO_SUPRESS_AN_ALARM_A: 'Cada política de alerta se puede establecer en diferentes niveles, y cada nivel corresponde a un período de alerta y período de repetición diferente.',
-  PROJECT_MONITOR_TARGET_TIPS: 'Una alerta puede especificar solo una política de tipo de recurso',
-  RULE_NAME_DESC: 'El nombre de la regla puede estar compuesto de cualquier carácter para ayudarlo a distinguir mejor los recursos.',
+  load1: 'CPU load average 1 minute',
+  load5: 'CPU load average 5 minute',
+  load15: 'CPU load average 15 minute',
 
+  ALERT_TYPE: '{type} Alert',
+  ALERT_POLICY_TYPE: '{type} Alerting Policy',
+  ALERT_POLICY_DESC: 'Set alert rules',
+  ALERT_MESSAGE_DESC:
+    'Alerting messages are generated based on the monitoring metrics and the workload alert policies in the current project. They can help users detect problems and respond in time.',
+  ALERT_POLICY_TRIGGER_RULE:
+    'Trigger Rules: Any of the following conditions is met.',
+  ALERT_METRIC_NAME: 'Metric Name',
+  ALERT_COMMENT: 'Comment',
+  ALERT_COMMENT_DESC:
+    'Comments will be retained as records for alert processing, but not as a means of handling alerts. Record information will be used to check the processing status of the alerts.',
+  ALERT_POLICY_SETTING_TITLE:
+    'The relevant alerting policy is not set temporarily',
+  ALERT_POLICY_SETTING_DESC:
+    'You can find the abnormal condition of the resource in time by setting the alert rules',
+  ALERTING_POLICY_CREATE_DESC:
+    'You can find the abnormal condition of the resource in time by setting the alert rules',
+  'ALERTING-POLICY_BASEINFO_DESC':
+    'Set basic information about the alerting policy',
+  'ALERTING-POLICY_MONITORING-TARGET_DESC':
+    'Select the monitoring target of the alerting policy',
+  'ALERTING-POLICY_ALERTING-RULE_DESC': 'Set alert rules',
+  'ALERTING-POLICY_NOTIFICATION-RULE_DESC': 'Set notification rules',
+  TOTAL_POLICIES: 'Total {num} alerting policies',
+
+  PERIOD_OPTIONS:
+    '{value, plural, =1 {1 minute/period} other {# minutes/period}}',
+  CONSECUTIVE_OPTIONS: '{value} consecutive times',
+
+  MAX_SEND_NOT_LIMIT: 'Not limited',
+  MAX_SEND_COUNT: 'Resend up to {count}',
+  REPEAT_CUSTOM_TITLE: 'Customize Repetition Rules',
+  REPEAT_CUSTOM_DESC:
+    'Alerting messages will be pushed repeatedly at specific intervals according to their severity. The default policy will be adopted unless you make any change to it.',
+  REPEAT_INTERVAL_NOT_REPEAT: 'Not repeating',
+  REPEAT_INTERVAL_MINUTE: 'Alert once every {num} minutes',
+  REPEAT_INTERVAL_HOUR: 'Alert once every {num} hour',
+  REPEAT_INTERVAL_DAY: 'Alert once every {num} days',
+  REPEAT_INTERVAL_EXP: 'Incremental exponential period',
+  REPEAT_RULE_EXP_TIP:
+    'Periodic index increment notification: When the alert duration reaches 1, 2, 4, 8, 16, 32... times of the alert statistics period, an alert notification is sent.',
+  REPEAT_RULE_TIPS:
+    'Alerting messages will be pushed repeatedly according to their severity. By default, <strong>Critical alert is repeated every 30 minutes</strong> / <strong>Major alert is repeated every 2 hours</strong> / <strong>Minor alert is not repeated</strong>.<br> If you need to modify the repetition rules, please customize the settings in Notification Rule section (next section).',
+
+  NOTIFY_TIME_LABEL: 'Effective Notification Time Range',
+  NOTIFY_TIME_TIP:
+    'You can set an effective time range for an alerting message.',
+  NOTIFY_LIST_ADD_TIP: 'Press the enter button or click to add',
+  NOTIFY_LIST_INPUT_ERRPR_TIP:
+    'The input format is incorrect. Please enter the correct email address.',
+  NOTIFY_LIST_INPUT_PLACEHOLDER:
+    'Please enter the email address of the member to be notified.',
+  NOTIFY_CURRENT_COUNT: 'Notification times: {count} in total',
+  RESOURCE_SELECTOR_FORM_TIP: 'Please fill in the label selector',
+  RESOURCE_NODE_FORM_TIP: 'Please select a cluster node',
+  RESOURCE_WORKLOAD_FORM_TIP: 'Please select a workload',
+  SEVERITY_MSG_FIXED_MINUTES: 'Notify every {count} minutes',
+  SEVERITY_MSG_NO_LIMIT: 'Not limited',
+  SEVERITY_MSG_EXP: 'Notice by exponential period',
+  SEVERITY_MSG_NOT_REPEAT: 'No repeat notification',
+  ALERT_RULE_STATUS_OCCURRED: 'Alerting',
+  ALERT_RULE_STATUS_NO_OCCURRED: 'Not alert yet',
+  SENT_RULE_TIME_TITLE: 'Effective Time Period',
+  SENT_RULE_CHANNEL_TITLE: 'Notification Method',
+
+  REQUESTS_FOR_TRIGGER_AN_ALARM_Q:
+    'Prerequisites for triggering an alerting message?',
+  REQUESTS_FOR_TRIGGER_AN_ALARM_A:
+    'You need to set an alerting policy for a resource. When a certain indicator of a resource reaches the threshold of the alerting policy, the message is triggered and pushed.',
+  REQUESTS_FOR_PUSH_AN_ALARM_Q:
+    'Prerequisites for a push message of alerting policy?',
+  REQUESTS_FOR_PUSH_AN_ALARM_A:
+    'The platform administrator is required to set up the mail server.',
+  HOW_TO_SUPRESS_AN_ALARM_Q: 'How to suppress alerting messages?',
+  HOW_TO_SUPRESS_AN_ALARM_A:
+    'Each alerting policy can be set at different levels, and each level corresponds to a different alert period and repetition period.',
+
+  PROJECT_MONITOR_TARGET_TIPS:
+    'An alert can specify only one resource type policy',
+
+  RULE_NAME_DESC:
+    'The rule name can be composed of any character to help you better distinguish resources.',
 }
