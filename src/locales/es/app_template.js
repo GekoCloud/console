d92201c350f08a1efcf26cc139a8b644279f2958
juan-vg <juan@geko.cloud>
2020-07-20 19:33:56 +0200

    Translate 'es/app_template.js'

--- src/locales/es/app_template.js
+++ src/locales/es/app_template.js
@@ -17,17 +17,17 @@
  */
 
 export default {
-  'App README': 'App README',
-  'Configuration Files': 'Configuration Files',
-  'Deploy Successfully': 'Deploy Successfully',
-  'Edit by YAML': 'Edit by YAML',
-  'The app has no chart file.': 'The app has no chart file.',
-  'The app has no documentation.': 'The app has no documentation.',
-  'The version has no documentation.': 'The version has no documentation.',
-  'Update Successfully': 'Update Successfully',
+  'App README': 'LEEME (README) de Aplicación',
+  'Configuration Files': 'Ficheros de Configuración',
+  'Deploy Successfully': 'Desplegado Satisfactoriamente',
+  'Edit by YAML': 'Editar YAML',
+  'The app has no chart file.': 'La aplicación no tiene un fichero Chart',
+  'The app has no documentation.': 'La aplicación no tiene documentación.',
+  'The version has no documentation.': 'La versión no tiene documentación.',
+  'Update Successfully': 'Subida Satisfactoriamente',
 
   APP_TEMPLATE_DESCRIPTION_TITLE: 'Implementar aplicaciones empaquetadas',
-  APP_TEMPLATE_DESCRIPTION_ARTICLE: 'KubeSphere visualiza plantillas de aplicaciones y proporciona funciones de implementación y administración, lo que permite a los usuarios implementar aplicaciones rápidamente con un solo clic en función de las plantillas de la aplicación.',
-  TOTAL_APP_REPOS: 'Total de {num} repositorios de aplicaciones',
+  APP_TEMPLATE_DESCRIPTION_ARTICLE: 'KubeSphere visualiza plantillas de aplicaciones y proporciona funciones de implementación y administración, lo que permite a los usuarios implementar aplicaciones rápidamente con un solo click en función de las plantillas de la aplicación.',
+  TOTAL_APP_REPOS: 'Total de {num} repositorios de Aplicaciones',
 
 }
