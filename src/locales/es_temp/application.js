54dd8ee0825a3f9d138930643b2c60a0e7202771
Xavi <xavi@geko.cloud>
2020-07-21 11:25:28 +0200

    Translate application.js, pending translation review

---
+++ src/locales/es_temp/application.js
@@ -0,0 +1,225 @@
+/*
+ * This file is part of KubeSphere Console.
+ * Copyright (C) 2019 The KubeSphere Console Authors.
+ *
+ * KubeSphere Console is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KubeSphere Console is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with KubeSphere Console.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+export default {
+  'According to the HTTP header': 'De acuerdo con el encabezado HTTP',
+  'Add Component': 'Agregar componente',
+  'Add Component Successfully': 'Añadir componente con éxito',
+  'Add Internet access rule for the application':
+    'Agregar regla de acceso a Internet para la aplicación',
+  'Add New Component': 'Agregar nuevo componente',
+  'Add Service': 'Agregar servicio',
+  'Add stateful or stateless services':
+    'Agregar servicios con estado o sin estado',
+  'App Description': 'Descripción de la aplicación',
+  'App store deployment': 'Implementación de la tienda de aplicaciones',
+  'App Template': 'Plantilla de la aplicación',
+  'App Types': 'Tipos de aplicaciones',
+  APP_ICON_TIP: 'APP_ICON_TIP',
+  application: 'solicitud',
+  'Application Component': 'Componente de aplicación',
+  'Application Components': 'Componentes de la aplicación',
+  'Application components combine workloads and services as components in applications':
+    'Los componentes de la aplicación combinan cargas de trabajo y servicios como componentes en las aplicaciones.',
+  'Application Governance': 'Gobierno de aplicación',
+  'Application governance is not enabled':
+    'El gobierno de la aplicación no está habilitado',
+  'Application Icon': 'Ícono de aplicación',
+  'Application Name': 'Nombre de la aplicación',
+  'Application Route': 'Ruta de solicitud',
+  'Application Type': 'tipo de aplicacion',
+  'Application Version(Optional)': 'Versión de aplicación (opcional)',
+  APPLICATION_YAML_DESC: 'APPLICATION_YAML_DESC',
+  Applications: 'Aplicaciones',
+  'Base ejection time (s)': 'Tiempo de eyección base (s)',
+  'Based on HTTP cookie': 'Basado en cookie HTTP',
+  'Based on HTTP header': 'Basado en el encabezado HTTP',
+  'Build an app by app template':
+    'Crear una aplicación por plantilla de aplicación',
+  'Build an app by services': 'Crea una aplicación por servicios',
+  'Called Depth': 'Profundidad llamada',
+  'Called Services': 'Servicios llamados',
+  'Choose existing services or create new service components to build an app':
+    'Elija servicios existentes o cree nuevos componentes de servicio para crear una aplicación',
+  'Circuit Breaker': 'Cortacircuitos',
+  'Cluster Select': 'Selección de clúster',
+  'Cluster Selection': 'Selección de clúster',
+  'Component Version': 'Versión componente',
+  'Composing App': 'Aplicación de composición',
+  'Connection pool management': 'Gestión de agrupación de conexiones',
+  'Connection timeout': 'El tiempo de conexión expiro',
+  'Continuous error response (5xx) number':
+    'Número de respuesta de error continuo (5xx)',
+  'Create Application by Service': 'Crear aplicación por servicio',
+  'Create Composing Application': 'Crear aplicación de composición',
+  'Current Version': 'Versión actual',
+  Deploy: 'Desplegar',
+  'Deploy App': 'Implementar aplicación',
+  'Deploy applications with one-click application templates provided by Kubesphere':
+    'Implemente aplicaciones con plantillas de aplicaciones de un solo clic proporcionadas por Kubesphere',
+  'Deploy New Application': 'Implementar nueva aplicación',
+  'Deploy sample app Bookinfo': 'Implementar la aplicación de ejemplo Bookinfo',
+  'Deployed Applications': 'Aplicaciones desplegadas',
+  'Edit Component': 'Editar componente',
+  'From App Store': 'Desde la tienda de aplicaciones',
+  'From App Templates': 'Desde plantillas de aplicaciones',
+  'From third party Helm': 'Del timón de terceros',
+  'From workspace': 'Del espacio de trabajo',
+  'Hash based on a specific HTTP header.':
+    'Hash basado en un encabezado HTTP específico.',
+  'Hash based on HTTP cookie.': 'Hash basado en cookie HTTP.',
+  'Hash based on the source IP address.':
+    'Hash basado en la dirección IP de origen.',
+  'How to use Application Governance': 'Cómo usar el gobierno de aplicaciones',
+  'If the maximum number of requests in the backend connection is set to 1, the keep alive feature is disabled.':
+    'Si el número máximo de solicitudes en la conexión de back-end se establece en 1, la función mantener vivo está deshabilitada.',
+  'If you need to access applications by application route, add routing rules':
+    'Si necesita acceder a las aplicaciones por ruta de aplicación, agregue reglas de enrutamiento',
+  'Inspection period (unit: s)': 'Período de inspección (unidad: s)',
+  'Invalid version': 'Versión inválida',
+  'Load balance algorithm': 'Algoritmo de equilibrio de carga',
+  Log: 'Iniciar sesión',
+  'Max number of connections': 'Número máximo de conexiones',
+  'Max number of requests per connection':
+    'Número máximo de solicitudes por conexión',
+  'Max request retries': 'Reintentos de solicitud máx.',
+  'Maximum pending requests': 'Máximo de solicitudes pendientes',
+  'Maximum requests': 'Solicitudes máximas',
+  Method: 'Método',
+  'Microservice enabled': 'Microservicio habilitado',
+  'Microservice not enabled': 'Microservicio no habilitado',
+  'No Components': 'Sin componentes',
+  'No result found': 'No se han encontrado resultados',
+  Off: 'Off',
+  On: 'On',
+  'Please finish the sub form first':
+    'Por favor termine el sub formulario primero',
+  'Please input an application name':
+    'Por favor ingrese un nombre de aplicación',
+  'Please input component version':
+    'Por favor ingrese la versión del componente',
+  'Please save the current form first':
+    'Por favor, guarde el formulario actual primero',
+  'Please try other query conditions':
+    'Por favor, intente otras condiciones de consulta',
+  'Pod isolation ratio (unit: %)':
+    'Relación de aislamiento de la vaina (unidad:%)',
+  Process: 'Proceso',
+  Receive: 'Recibir',
+  Rollback: 'Retroceder',
+  'Sample apps can help you get started with app creation and app governance':
+    'Las aplicaciones de muestra pueden ayudarlo a comenzar con la creación y el gobierno de la aplicación',
+  Send: 'Enviar',
+  'Service components should not be empty':
+    'Los componentes de servicio no deben estar vacíos.',
+  'Service Ports': 'Puertos de servicio',
+  SERVICE_PORT_NAME_DESC: 'SERVICE_PORT_NAME_DESC',
+  'Services & Operations': 'Servicios y operaciones',
+  'Session retention': 'Retención de la sesión',
+  'Specify an application version': 'Especificar una versión de la aplicación',
+  'Success rate': 'Tasa de éxito',
+  Tags: 'Etiquetas',
+  'TCP - Inbound Traffic': 'TCP - Tráfico entrante',
+  'TCP - Outbound Traffic': 'TCP - Tráfico saliente',
+  'TCP connection timeout.': 'Tiempo de espera de conexión TCP.',
+  'Temporarily unable to use tracing':
+    'No se puede usar el rastreo temporalmente',
+  'Temporarily unable to use traffic management':
+    'No se puede utilizar temporalmente la gestión del tráfico.',
+  'The app has not received the request for a long time, please visit the app and try traffic management':
+    'La aplicación no ha recibido la solicitud durante mucho tiempo, visite la aplicación e intente administrar el tráfico',
+  'The maximum number of HTTP1 or TCP connections to the target host.':
+    'El número máximo de conexiones HTTP1 o TCP al host de destino.',
+  'The maximum number of retries to the target host within the specified time.':
+    'El número máximo de reintentos al host de destino dentro del tiempo especificado.',
+  'The number of consecutive 5xx errors in one inspection cycle':
+    'El número de errores 5xx consecutivos en un ciclo de inspección',
+  'The response code will be filtered in the inspection cycle.':
+    'El código de respuesta se filtrará en el ciclo de inspección.',
+  TOTAL_COLLECTIONS: 'TOTAL_COLLECTIONS',
+  Traffic: 'Tráfico',
+  'Traffic (requests per second)': 'Tráfico (solicitudes por segundo)',
+  'Traffic Entry': 'Entrada de tráfico',
+  'Traffic Management': 'La gestión del tráfico',
+  'Traffic Monitoring': 'Monitoreo de tráfico',
+  Upgrade: 'Potenciar',
+  'Workload Type': 'Tipo de carga de trabajo',
+  TOTAL_APPS: 'Total de {num} aplicaciones',
+  APPLICATIONS_DESC:
+    'Una aplicación proporciona a los usuarios funciones comerciales integrales en un solo paquete. Las plantillas de aplicación en KubeSphere se basan en la especificación de empaquetado de Helm. Se entregan a través de un repositorio Helm público o privado unificado. Una aplicación está compuesta por uno o más objetos de Kubernetes, que incluyen cargas de trabajo, servicios e ingreso.',
+  APP_DEPLOYMENT_DESC:
+    'La tecnología de empaquetado de software liviano, portátil y autónomo permite que las aplicaciones se ejecuten en casi cualquier lugar de la misma manera.',
+  LB_ALG_DESC:
+    'Admite algoritmos de equilibrio de carga estándar </br> ROUND_ROBIN ： Sondeo, el algoritmo de equilibrio de carga predeterminado. </br> LEAST_CONN: seleccione aleatoriamente dos hosts sanos y luego seleccione un host con menos enlaces de los dos hosts seleccionados. </br> ALEATORIO: Elija uno al azar de todos los hosts sanos.',
+  LB_ROUND_ROBIN: 'ROUND_ROBIN',
+  LB_LEAST_CONN: 'LEAST_CONN',
+  LB_RANDOM: 'ALEATORIO',
+  CLUSTER_NAME_DESC:
+    'Solo puede contener letras minúsculas, números y guiones (&quot;-&quot;), y debe comenzar con una letra minúscula y terminar con un número o letra minúscula. La longitud máxima de caracteres se establece en 14.',
+  CLUSTER_ALIAS_DESC:
+    'El alias puede estar compuesto de cualquier carácter para ayudarlo a distinguir mejor los recursos y admitir nombres chinos.',
+  COMPONENT_VERSION_DESC:
+    'Para la aplicación de la gobernanza para distinguir entre componentes. Solo puede contener letras minúsculas y números. La longitud máxima de caracteres se establece en 16.',
+  APP_WORKLOAD_TYPE_DESC:
+    'Admite servicios sin estado (implementación) y servicios con estado (StatefulSet)',
+  SERVICE_DEPLOYMENT: 'Servicio sin estado (implementación)',
+  SERVICE_STATEFULSET: 'Servicio con estado (StatefulSet)',
+  CONNECTION_POOL_TIP:
+    'Cree un número fijo de objetos de conexión para la aplicación, guárdelos en el grupo para su reutilización. El grupo existente está disponible para cada acceso y se devuelve al grupo después de su uso.',
+  APP_GOVERNANCE_DESC:
+    'Si Application Governance está habilitado, el contenedor Istio-proxy se inyectará como SideCar en cada componente. <a href="https://istio.io/docs/setup/kubernetes/additional-setup/sidecar-injection/" target="_blank">LEE MAS</a>',
+  POD_ISOLATION_RATIO_DESC:
+    '% Máximo de Pod para el servicio ascendente que se puede expulsar. Utiliza redondeo ascendente, cuando se establece en 13%, aislará hasta 2 instancias si existen 10 instancias.',
+  BASE_EJECTION_TIME_DESC:
+    'Duración mínima de expulsión. Un host permanecerá expulsado durante un período igual al producto de duración mínima de expulsión y la cantidad de veces que se expulsó el host.',
+  CIRCUIT_DESC:
+    'El corte de circuito es un mecanismo de protección de conexión de microservicio que aborda el efecto de avalancha. Cuando un microservicio de la conexión de abanico no está disponible o el tiempo de respuesta es demasiado largo, el servicio se degradará y la llamada al microservicio del nodo se interrumpirá y la respuesta de error se devolverá rápidamente. Después de detectar que la respuesta del microservicio es normal, se reanudará la conexión.',
+  'Last {num} records': 'Últimos {num} registros',
+  'Last {hour} hour': 'Última {hora} hora',
+  'Last {hour} hours': 'Últimas {hora} horas',
+  'Last {day} days': 'Últimos {días} días',
+  WORKLOAD_NAME_EXIST: 'La carga de trabajo {nombre} existe',
+  APPLICATION_TYPE_DESC:
+    'KubeSphere admite implementaciones de aplicaciones (basadas en Helm) desde App Store, así como Application CRD.',
+  HOW_TO_USE_APPLICATION_GOVE_Q: '¿Cómo usar el gobierno de aplicaciones?',
+  HOW_TO_USE_APPLICATION_GOVE_A:
+    'Cree una aplicación de composición y habilite la gestión de servicios para cada servicio antes de utilizar la Gestión de aplicaciones.',
+  FROM_APP_STORE_DESC:
+    'Desde la App Store oficial de KubeSphere con aplicaciones de alta calidad y despliegues fáciles.',
+  FROM_APP_TEMPLATES_DESC:
+    'Desde las plantillas de aplicación del espacio de trabajo y las plantillas de aplicaciones Helm de terceros del Repositorio de aplicaciones.',
+  COMPOSING_APP_DESC:
+    'Puede publicar servicios para crear aplicaciones a través de la orquestación de recursos (compatible con Application Governance).',
+  APP_TEMPLATES_MODAL_DESC:
+    'La plantilla de la aplicación proviene del espacio de trabajo y las plantillas de la aplicación Helm de terceros. Admite la implementación con un clic y se puede visualizar en KubeSphere para mostrar y proporcionar implementaciones y funciones de administración.',
+  TIP_APP_TYPE:
+    'KubeSphere admite el despliegue de aplicaciones (basado en Helm) desde tiendas de aplicaciones y repositorios de aplicaciones, así como aplicaciones caseras (Application CRD).',
+  TIP_APP_GOVERNANCE:
+    'Usar App Governance requiere crear una aplicación homebrew y activar la gobernanza de servicio para cada servicio',
+  APP_REPOS_DESC:
+    'El repositorio de aplicaciones proviene de un repositorio Helm Chart Repo de terceros, que muestra visualmente y proporciona capacidades de implementación y administración en KubeSphere. Los usuarios pueden implementar rápidamente aplicaciones con un solo clic según las plantillas en el repositorio de aplicaciones.',
+  SEARCH_TIPS: 'Puede filtrar según las condiciones relevantes',
+  APPLICATION_BASEINFO_DESC:
+    'Información básica de la aplicación (como la descripción del nombre)',
+  TRAFFIC_MANAGEMENT_NO_MICROSERVICE_TIP:
+    'La gobernanza del tráfico depende del módulo de microservicio, el clúster actual no habilita el módulo de microservicio',
+  TRACING_NO_MICROSERVICE_TIP:
+    'El seguimiento depende del módulo de microservicio, el clúster actual no habilita el módulo de microservicio',
+  APPLICATION_SERVICE_DESC:
+    'Puede configurar diferentes componentes de servicio según el tipo de servicio en una aplicación. Los servicios con estado y sin estado son compatibles.',
+}
