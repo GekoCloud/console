fdfba4dc2758e16aa3da42bc4e3c56301c5040de
David Pujadas <dpujadas@gmail.com>
2020-07-23 17:34:19 +0200

    Review configmap.js

--- src/locales/es/configmap.js
+++ src/locales/es/configmap.js
@@ -24,12 +24,12 @@ export default {
   configmaps: 'configmaps',
   ConfigMaps: 'ConfigMaps',
   'Enter the value of the configuration map entry or use the contents of the file':
-    'Introduce el valor de la entrada del mapa de configuración o use el contenido del archivo',
+    'Introduce el valor de la entrada del configmap o utiliza el contenido del archivo',
   'Modify Config': 'Modificar configuración',
   'The unique key value of this configuration map entry':
-    'El valor clave único de esta entrada del mapa de configuración',
+    'La única clave / valor de esta entrada del configmap',
   CONFIGMAP_CREATE_DESC:
-    'This is a test Un ConfigMap es un objeto utilizado para almacenar variables de configuración comunes, lo que permite la gestión unificada de variables de entorno para diferentes módulos en un sistema distribuido.',
+    'Un ConfigMap es un objeto utilizado para almacenar variables de configuración comunes, lo que permite la gestión unificada de variables de entorno para diferentes módulos en un sistema distribuido.',
   CONFIGMAP_DESC:
     'Un ConfigMap a menudo se usa para almacenar la información de configuración necesaria para cargas de trabajo. Muchas aplicaciones leerán la información de archivos de configuración, de parámetros de la línea de comandos o de variables de entorno.',
   CONFIGMAPS_BASEINFO_DESC: '',
