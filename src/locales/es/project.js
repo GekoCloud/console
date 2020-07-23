bb63c2e09db1aaf6b966a48da938b50e227ed787
Marc Lopez <marclv@gmail.com>
2020-07-23 18:28:08 +0200

    reviewed files

--- src/locales/es/project.js
+++ src/locales/es/project.js
@@ -51,12 +51,12 @@ export default {
   'Edit Resource Default Request':
     'Editar solicitud predeterminada de recursos',
   'Empty value means no limit, CPU 1 Core = 1000m':
-    'El valor vacío significa que no hay límite, CPU 1 Core = 1000m',
-  'Enter DevOps Project': 'Introduce al proyecto DevOps',
+    'El valor vacío significa que no hay límite, 1 Core de CPU = 1000m',
+  'Enter DevOps Project': 'Introduce el proyecto DevOps',
   'Enter Project': 'Entrar al proyecto',
-  FED_HOST_NAMESPACE_TIP: 'FED_HOST_NAMESPACE_TIP',
+  FED_HOST_NAMESPACE_TIP: 'Por favor, no manipules los recursos de este proyecto puesto que pertence a un proyecto multicluster',
   'Gateway Info': 'Información de puerta de enlace',
-  'Gateway Not Set': 'Gateway no establecido',
+  'Gateway Not Set': 'Puerta de enlace no establecida',
   'Help Information': 'Información de ayuda',
   'How to invite other members to the current project?':
     '¿Cómo invitar a otros miembros al proyecto actual?',
@@ -68,7 +68,7 @@ export default {
   'Invite Members to the Project': 'Invitar miembros al proyecto',
   jobs: 'trabajos',
   'limits.cpu': 'limit.cpu',
-  'limits.memory': 'límites.memoria',
+  'limits.memory': 'limits.memoria',
   LoadBalancer: 'LoadBalancer',
   'Manage Project': 'Gestionar proyecto',
   'Member Name': 'Nombre de miembro',
@@ -77,24 +77,24 @@ export default {
   'Modify Member Role': 'Modificar rol de miembro',
   'Modify Members Role': 'Modificar rol de miembros',
   'Multi-cluster Project': 'Proyecto de clúster múltiple',
-  'Multi-cluster Projects': 'Proyectos multi-cluster',
-  MULTI_CLUSTER_PROJECT_DELETE_TIP: 'MULTI_CLUSTER_PROJECT_DELETE_TIP',
+  'Multi-cluster Projects': 'Proyectos de clústeres múltiples',
+  MULTI_CLUSTER_PROJECT_DELETE_TIP: 'Borrar un proyecto multi-cluster borra también el mismo proyecto en el host de cluster.</br>Por favor, introduce el nombre {type} <strong>{resource}</strong> para estar seguro de que entiendes el riesgo de esta operación.',
   'Network Isolation': 'Aislamiento de red',
   'No Platform Manage Authorization':
     'Sin autorización de gestión de plataforma',
   'Not Assigned': 'No asignado',
-  Opened: 'Abrió',
+  Opened: 'Abierto',
   'Please input project name': 'Por favor introduce el nombre del proyecto',
   pods: 'pods',
   'Project Info': 'Información del proyecto',
-  'Project Manager': 'Gerente de proyecto',
+  'Project Manager': 'Gestor de proyecto',
   'Project Member': 'Miembro del proyecto',
   'Project Members': 'Miembros del proyecto',
   'project members': 'miembros del proyecto',
   'Project Name': 'Nombre del proyecto',
   'Project name exists': 'El nombre del proyecto existe',
   'Project name exists on host cluster':
-    'El nombre del proyecto existe en el clúster host',
+    'El nombre del proyecto existe en el clúster',
   'Project Overview': 'Descripción del proyecto',
   'Project Placement': 'Colocación del proyecto',
   'Project Quota': 'Cuota de proyecto',
@@ -107,7 +107,7 @@ export default {
   'Remove Member': 'Eliminar miembro',
   'Remove Members': 'Eliminar miembros',
   'requests.cpu': 'request.cpu',
-  'requests.memory': 'solicitudes.memoria',
+  'requests.memory': 'requests.memory',
   'Resource Quota': 'Cuota de recursos',
   'Resource Type': 'Tipo de recurso',
   'Select a user of the workspace as the manager of the project.':
@@ -118,7 +118,7 @@ export default {
   Service: 'Servicio',
   'Set Gateway': 'Establecer puerta de enlace',
   StatefulSet: 'StatefulSet',
-  statefulsets: 'conjuntos de estado',
+  statefulsets: 'statefulsets',
   'Target Workspace': 'Espacio de trabajo de destino',
   Terminating: 'Terminando',
   Usage: 'Uso',
@@ -132,7 +132,7 @@ export default {
   PROJECT_ADVANCE_DESC:
     'Establecer la solicitud de recursos predeterminada del proyecto',
   PROJECT_NAME_DESC:
-    'El nombre del proyecto solo puedes contener letras minúsculas, números y guiones (&quot;-&quot;).',
+    'El nombre del proyecto solo puedes contener letras minúsculas, números y guiones ("-").',
   PROJECT_BASEINFO_DESC: 'Configuración de información básica del proyecto',
   PROJECT_TYPES_PROJECT_TITLE: 'Crear un proyecto de recursos',
   PROJECT_TYPES_PROJECT_DESC:
@@ -141,22 +141,22 @@ export default {
   PROJECT_TYPES_DEVOPS_DESC:
     'Cree y pruebe proyectos de software de forma continua y automática.',
   PROJECT_ASSIGN_DESC:
-    'Una vez que el proyecto se asigna a un espacio de trabajo, no se puedes cambiar.',
+    'Una vez que el proyecto se asigna a un espacio de trabajo ya no se puedes cambiar.',
   INVITE_MEMBER_DESC:
     'Puedes invitar a nuevos miembros para ayudar a su proyecto.',
   INVITE_MEMBER_DESC_DEVOPS:
-    'Puedes invitar a nuevos miembros para que ayuden en su proyecto DevOps.',
+    'Puedes invitar a nuevos miembros para que ayuden en tu proyecto DevOps.',
   INVITE_MEMBER_SEARCH_PLACEHODLER:
     'Introduce una dirección de correo electrónico para invitar miembros',
-  INVITE_MEMBER_CHOOSE_ROLE_TIP: 'Asigne un rol a este miembro',
+  INVITE_MEMBER_CHOOSE_ROLE_TIP: 'Asigna un rol a este miembro',
   DELETE_MEMBER_TIP:
-    '¿Estás seguro de eliminar el miembro <strong>{name}</strong> ? El miembro no puedes acceder a este proyecto una vez eliminado.',
+    '¿Estás seguro de eliminar el miembro <strong>{name}</strong> ? El miembro no podrá acceder a este proyecto una vez eliminado.',
   PROJECT_ADMIN_DESC:
     'Puedes especificar un miembro del proyecto como administrador.',
   DEVOPS_ADMIN_DESC:
     'Puedes especificar un miembro del proyecto DevOps como administrador.',
   PROJECT_INTERNET_ACCESS_DESC:
-    'Antes de crear una ruta, debe habilitar el portal de acceso a Internet (es decir, la puerta de enlace). Este paso es crear un controlador de enrutamiento correspondiente para reenviar la solicitud al servicio de back-end correspondiente.',
+    'Antes de crear una ruta, debe habilitar el portal de acceso a Internet (es decir, la puerta de enlace). Este paso crea un controlador de enrutamiento correspondiente para reenviar la solicitud al servicio de back-end correspondiente.',
   DELETE_INTERNET_ACCESS_TITLE:
     '¿Está seguro de eliminar la configuración de acceso a la red externa?',
   DELETE_INTERNET_ACCESS_DESC:
@@ -165,11 +165,11 @@ export default {
   NO_RELATE_PROJECTS_DESC:
     'Puedes crear o ponerse en contacto con el gerente del proyecto para invitarlo al proyecto a comenzar su trabajo.',
   DELETE_PROJECT_TIP:
-    '¿Está seguro de eliminar el proyecto <strong>{resource}</strong> ? Después de la eliminación, no podrá recuperarla, y los recursos del proyecto también serán destruidos.',
+    '¿Está seguro de eliminar el proyecto <strong>{resource}</strong> ? Después de la eliminación no podrás recuperarlo y los recursos del proyecto también serán destruidos.',
   DEFAULT_RESOURCE_UNIT_DESC:
     'El valor de CPU indica el número de núcleos de CPU cuando no tiene unidad. 1 núcleo = 1000m',
   DEFAULT_RESOURCE_ALERT:
-    'Cuando crea una carga de trabajo, esta configuración se utilizará de manera predeterminada si el límite de recursos y la solicitud de la carga de trabajo no están establecidos. Si no hay requisitos especiales, mantenga esta configuración predeterminada sin cambios.',
+    'Cuando creas una carga de trabajo, esta configuración se utilizará de manera predeterminada si el límite de recursos y la solicitud de la carga de trabajo no están establecidos. Si no hay requisitos especiales, mantenga esta configuración predeterminada sin cambios.',
   QUOTA_EDIT_TIP:
     'Si el valor está vacío, la cuota de recursos no estará limitada.',
   HOW_TO_USE_QUOTA_Q: '¿Cómo usar las cuotas?',
@@ -182,7 +182,7 @@ export default {
   WHAT_IS_INTERNET_GATEWAY:
     '¿Qué es una puerta de enlace de acceso a Internet?',
   WHAT_IS_COLLECT_FILE_LOG_A:
-    'La ruta de registro en el volumen montado en el contenedor se da en un patrón global. La ruta de registro se puedes configurar en la carga de trabajo para recopilar registros. El administrador debe habilitar la recopilación de registros de disco de antemano.',
+    'La ruta de registro en el volumen montado en el contenedor se da en un patrón global. La ruta de registro se puede configurar en el workload para recopilar registros. El administrador debe habilitar la recopilación de registros de disco de antemano.',
   HOW_TO_INVITE_MEMBER_Q: '¿Cómo invitar a miembros?',
   HOW_TO_INVITE_MEMBER_A:
     'Los administradores del proyecto o los usuarios con privilegios de invitación a miembros pueden invitar a miembros del espacio de trabajo actual a unirse al proyecto.',
@@ -201,7 +201,7 @@ export default {
   PROJECT_ADVANCED_SETTINGS_DESC:
     'Este módulo le permite configurar el acceso a la puerta de enlace para direcciones externas y el gobierno del servicio. Además, también puedes configurar la colección de registros en el disco.',
   PROJECT_TYPES_Q:
-    '¿Cómo se puedes acceder a los servicios del proyecto a través de la red externa?',
+    '¿Cómo puedes acceder a los servicios del proyecto a través de la red externa?',
   PROJECT_TYPES_A:
     'La puerta de enlace del proyecto es responsable de crear el controlador de enrutamiento de la aplicación correspondiente para reenviar la solicitud al servicio de back-end correspondiente. Después de abrir la puerta de enlace del proyecto, el servicio puedes exponerse a la red externa a través de Ingress.',
   NAME_EXIST_IN_CLUSTER: 'El nombre existe en {cluster}',
@@ -213,9 +213,9 @@ export default {
   FEDPROJECT_CANNOT_DEPLOY_APP_TIP:
     'No se pueden implementar aplicaciones en proyectos de múltiples clústeres.',
   MULTI_CLUSER_PROJECT_TIP:
-    'El proyecto multi-cluster actual se ejecuta en distintos clústers. Puedes saltar entre clústers para comprobar los parámetros de este proyecto en cada uno de ellos.',
+    'El proyecto multi-clúster actual se ejecuta en distintos clústers. Puedes saltar entre clústers para comprobar los parámetros de este proyecto en cada uno de ellos.',
   MULTI_CLUSER_RESOURCE_TIP:
-    'El recurso multi-cluster actual se ejecuta en distintos clústers. Puedes saltar entre clústers para comprobar los parámetros de este recurso en cada uno de ellos.',
+    'El recurso multi-clúster actual se ejecuta en distintos clústers. Puedes saltar entre clústers para comprobar los parámetros de este recurso en cada uno de ellos.',
   MULTI_CLUSTER_PROJECT_CREATE_DESC:
-    'Un proyecto multi-cluster se ejecuta sobre distintos clústers a la vez, lo que ayuda a que puedas construir un entorno de contenedores para una rápida iteración de aplicaciones y conseguir alta disponibilidad.',
+    'Un proyecto multi-clúster se ejecuta sobre distintos clústers a la vez, lo que ayuda a que puedas construir un entorno de contenedores para una rápida iteración de aplicaciones y conseguir alta disponibilidad.',
 }
